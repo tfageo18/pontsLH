903ea8e4388835edcb46ddec5ada443ef5c41e09
tfageo18 <thomas.fageol@gmail.com>
2023-03-16 22:56:04 +0100

    Correction chemin pour les fichiers de confction de config et le CSS.

--- about.php
+++ about.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <?php 
-    include('E:/laragon/www/ponts/fonctions/fonctions.php');
-    include('E:/laragon/www/ponts/app.config.php');
+    include('./fonctions/fonctions.php');
+    include('./app.config.php');
 ?>
 <html lang="fr">
     <head>
         <title>Pont du port du Havre</title>
-        <link href="/styles/styles.css" rel="stylesheet" type="text/css">
+        <link href="./styles/styles.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <ul>
@@ -16,7 +16,7 @@
         </ul>
         <div class="contenu">
             <p>Bienvenue sur cette application qui vous permet d'afficher uniquement les status des ponts de votre choix au Havre</p>
-            <p>Cette application est gratuite et utilise les données du site <a href="https://havre-port.com">Havre-Port.com</a></p>
+            <p>Cette application est gratuite et utilise les données du site <a href="https://www.havre-port.com/" target="_blank">Havre-Port.com</a></p>
             <p>Il vous est possible de récupérer les sources directement sur GitHUB et de faire vivre l'application à votre guise.</p>
         </div>
         <?php include ("./footer.php"); ?>
